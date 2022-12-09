1dbd6d4d2055ae5ec6c830f0502414b6115d8125
Wesley Vercoutere <32158471+WesleyVercoutere@users.noreply.github.com>
2022-12-09 13:48:33 +0100

    Update AsyncAwaitTask

--- AsyncAwaitTask/Program.cs
+++ AsyncAwaitTask/Program.cs
@@ -1,2 +1,116 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.Run();
+        }
+    }
+
+    class App
+    {
+        private Food _fries = new Food("Fries", 5000);
+        private Food _meat = new Food("Meat", 10000);
+
+        public void Run()
+        {
+            MakeFoodSync();
+            Console.WriteLine("");
+            MakeFoodAsync();
+            Console.WriteLine("");
+            MakeFoodAsync2();
+            Console.WriteLine("");
+            MakeFoodAsync3();
+            Console.ReadLine();
+        }
+
+        public void MakeFoodSync()
+        {
+            var start = DateTime.Now;
+            Console.WriteLine("Start making food sync");
+
+            _fries.Make();
+            _meat.Make();
+
+            var end = DateTime.Now;
+            var total = end - start;
+            Console.WriteLine($"Making food sync takes {total} seconds");
+        }
+
+        public void MakeFoodAsync()
+        {
+            var start = DateTime.Now;
+            Console.WriteLine("Start making food async fout");
+
+            _fries.MakeAsync().Wait(); // Wacht gewoon tot task klaar is, geen winst
+            _meat.MakeAsync().Wait();
+
+            var end = DateTime.Now;
+            var total = end - start;
+            Console.WriteLine($"Making food async takes {total} seconds");
+        }
+
+        public void MakeFoodAsync2()
+        {
+            var start = DateTime.Now;
+            Console.WriteLine("Start making food async ook fout");
+
+            _fries.MakeAsync();
+            _meat.MakeAsync();
+
+            var end = DateTime.Now;
+            var total = end - start;
+            Console.WriteLine($"Making food async takes {total} seconds");
+
+            Task.Delay(_fries.MakeTime + _meat.MakeTime).Wait(); // Niet nodig maar anders staat output tussen MakeFoodAsync3()
+        }
+
+        public void MakeFoodAsync3()
+        {
+            var start = DateTime.Now;
+            Console.WriteLine("Start making food async");
+
+            Task makeMeat = _meat.MakeAsync();
+            Task.Delay(5000).Wait();
+            Task makeFries = _fries.MakeAsync();
+            
+            makeFries.Wait();
+            makeMeat.Wait();
+
+            var end = DateTime.Now;
+            var total = end - start;
+            Console.WriteLine($"Making food async takes {total} seconds");
+        }
+    }
+
+    class Food
+    {
+        public string FoodToMake { get; set; }
+        public int MakeTime { get; set; }
+
+        public Food(string foodToMake, int makeTime)
+        {
+            FoodToMake = foodToMake;
+            MakeTime = makeTime;
+        }
+
+        public void Make()
+        {
+            Console.WriteLine($"Start making {FoodToMake}.");
+            Task.Delay(MakeTime).Wait();
+            Console.WriteLine($"{FoodToMake} ready.");
+        }
+
+        public async Task MakeAsync()
+        {
+            Console.WriteLine($"Start making {FoodToMake}.");
+            await Task.Delay(MakeTime);   
+            Console.WriteLine($"{FoodToMake} ready.");
+        }
+    }
+}
